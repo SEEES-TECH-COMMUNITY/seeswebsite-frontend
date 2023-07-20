12e75c524a47ee1fdd59195103209f9cd4e81542
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2023-07-20 19:48:56 +0000

    feat: removed font options added

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -7,7 +7,7 @@ export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 const Button: FC<IButtonProps> = ({className, text,...props}) => {
   return (
-    <button {...props} className={`text-bold w-full rounded-lg border border-transparent bg-blue-600 py-3 pl-10 pr-12 font-sans text-xs text-white placeholder-opacity-70 focus:border-blue-500 focus:outline-none focus:ring-blue-500 ${className ?? ''}`}>
+    <button {...props} className={`text-bold w-full rounded-lg border border-transparent bg-blue-600 py-3 pl-10 pr-12 text-xs text-white placeholder-opacity-70 focus:border-blue-500 focus:outline-none focus:ring-blue-500 ${className ?? ''}`}>
       {text}
     </button>
   );
