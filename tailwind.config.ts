6e2af9776f07e44ffd0224b3371fb152c460bb33
Udoka <udochukwukaonyela@gmail.com>
2023-09-15 23:09:49 +0100

    feat@: adde dto configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,18 @@ export default {
     extend: {
       colors: {
         grey: {
+          800: "#AAA",
           700: "#A7B5C1",
+          650: "#6D6D6D",
           600: "#F3F3F3",
           400: "#FBFBFB",
         },
+        dark: {
+          900: "#000F1E",
+        },
         blue: {
           600: "#1573FF",
+          200: "#D8E7FF",
           placeholder: {
             600: "#002445",
           },
@@ -19,6 +25,14 @@ export default {
         red: {
           500: "#FF1515",
         },
+        green: {
+          800: "#249607",
+          300: "#D5FFCB",
+        },
+        yellow:{
+          800: "#F2A102",
+          300: "#FFFACB"
+        }
       },
       fontSize: {
         xxs: ".625rem",
