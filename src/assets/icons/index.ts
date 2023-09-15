2c3128f5c88847dc568b6f4927776a8aec2e40de
Udoka <udochukwukaonyela@gmail.com>
2023-09-15 22:42:00 +0100

    feat: exported icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,5 @@
 export { default as LockIcon } from './LockIcon.icon';
 export { default as EyeIcon } from './EyeIcon.icon';
 export { default as EmailIcon } from './EmailIcon.icon';
+export { default as AddIcon } from './AddIcon.icon';
+export  {default as FilterIcon} from './FilterIcon.icon';
