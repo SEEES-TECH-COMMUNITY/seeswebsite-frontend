cd21cc0c5ab30e29d08782d701ed333751cd67bd
Udoka <udochukwukaonyela@gmail.com>
2023-09-15 22:51:23 +0100

    feat: added redux provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
+/* eslint-disable react/prop-types */
+import * as React from 'react';
 import { type AppType } from "next/dist/shared/lib/utils";
+import { Provider } from 'react-redux';
 import "@src/styles/globals.css";
+import store from '@src/utils/services/store';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 };
 
 export default MyApp;
