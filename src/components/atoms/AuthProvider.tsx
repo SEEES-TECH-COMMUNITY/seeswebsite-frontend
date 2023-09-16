5758477146f7414280f7af0aaf172973ac4f61df
Udoka <udochukwukaonyela@gmail.com>
2023-09-16 20:49:28 +0100

    feat: created auth provider

---
+++ src/components/atoms/AuthProvider.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import { useAppDispatch } from '@src/utils/hooks/useRedux';
+import { useGetAccountQuery } from '@src/utils/services/ApiService';
+import { addUser } from '@src/utils/services/AppSlice';
+import React, { useEffect, type FC } from 'react';
+
+export interface IAuthProviderProps {
+}
+
+const AuthProvider: FC<IAuthProviderProps> = (props) => {
+  const { data } = useGetAccountQuery(undefined);
+ const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (data) {
+      dispatch(addUser(data));
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+  return (null);
+}
+export default AuthProvider;
