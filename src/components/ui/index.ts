b22403240f48c47ab33d5e9232eabbf00587724f
Udoka <udochukwukaonyela@gmail.com>
2023-09-16 20:49:09 +0100

    feat: exported ui componnets

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as PasswordInput } from './PasswordInput';
 export { default as TextInput } from './TextInput';
+export { default as SelectInput } from './SelectInput';
 export { default as Button } from './Button';
