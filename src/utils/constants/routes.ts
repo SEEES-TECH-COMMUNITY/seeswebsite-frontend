bbaa2b1e9445fae45398931fedcbde3815aa411a
Udoka <udochukwukaonyela@gmail.com>
2023-09-23 22:11:22 +0100

    feat: added new api routes

--- src/utils/constants/routes.ts
+++ src/utils/constants/routes.ts
@@ -4,11 +4,15 @@ enum ApiRoutes {
   logout = "/accounts/logout",
   complete_signup = "/accounts/complete-signup",
   get_account = "/accounts/get-account",
+  get_user = "/accounts/get-user",
+  get_all_user = "/accounts/get-all-users",
   refresh = "/accounts/refresh",
   confirm_email = "/accounts/confirmEmail/{confirmationCode}",
   resend_confirmation_email = "/accounts/resendEmailVerificationCode",
   reset_password =  "/api/v1/accounts/resetPassword/{token}",
   forgot_password =  "/accounts/forgotPassword?email={email}",
+  get_all_announcements = "/announcement/get-all-announcement",
+  get_announcement = "/announcement/get-announcement?announcementID={id}",
   
 }
 export default ApiRoutes;
